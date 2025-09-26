ce6853ff30b380acf84fe9273440d87b0f736fc1
Baron Roberts <baron@cthing.com>
2025-09-26 14:50:05 -0700

    Generate release candidate.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-version = ProjectVersion("3.0.0", BuildType.snapshot)
+version = ProjectVersion("3.0.0", BuildType.release)
 group = "org.cthing"
 description = "A Gradle plugin that provides support for the publishing of C Thing Software artifacts."
 
